74874bbdaa0b8872c3eb6cc925e951304be9e64f
wahyu <wahyusutejo1986@gmail.com>
2024-12-14 16:50:17 +0700

    remove cp docker-compose.override.yml.example docker-compose.override.yml     modified:   modules/opencti/opencti.sh

--- modules/opencti/opencti.sh
+++ modules/opencti/opencti.sh
@@ -6,7 +6,6 @@ install_opencti() {
     local OPENCTI_REPO="https://github.com/OpenCTI-Platform/docker.git"
     git clone $OPENCTI_REPO $BASE_DIR/opencti || error_handler "Cloning OpenCTI Repository"
     cd $BASE_DIR/opencti
-    cp docker-compose.override.yml.example docker-compose.override.yml
     docker-compose up -d || error_handler "Starting OpenCTI Containers"
     cd -
 }
