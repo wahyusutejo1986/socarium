7796cae3861d5b0f4308896445e0e44c9fae162e
wahyu <wahyusutejo1986@gmail.com>
2024-12-14 16:08:06 +0700

    remove "&& sudo apt upgrade" 	modified:   modules/prerequisites/prerequisites.sh

--- modules/prerequisites/prerequisites.sh
+++ modules/prerequisites/prerequisites.sh
@@ -2,7 +2,7 @@
 
 install_prerequisites() {
     echo "🛠 Installing prerequisites..."
-    sudo apt update && sudo apt upgrade -y || error_handler "System Update"
+    sudo apt update -y || error_handler "System Update"
     sudo apt install -y docker docker-compose git curl wget build-essential python3-pip || error_handler "Prerequisites Installation"
 }
 
