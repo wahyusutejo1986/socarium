931e9f310cd62d59deda2cc0693151e368e0ab1f
wahyu <wahyusutejo1986@gmail.com>
2024-12-14 17:12:22 +0700

    add sudo mv .env.sample .env add sudo docker-compose up -d     modified:   modules/opencti/opencti.sh

--- modules/opencti/opencti.sh
+++ modules/opencti/opencti.sh
@@ -6,6 +6,7 @@ install_opencti() {
     local OPENCTI_REPO="https://github.com/OpenCTI-Platform/docker.git"
     git clone $OPENCTI_REPO $BASE_DIR/opencti || error_handler "Cloning OpenCTI Repository"
     cd $BASE_DIR/opencti
-    docker-compose up -d || error_handler "Starting OpenCTI Containers"
+    sudo mv .env.sample .env
+    sudo docker-compose up -d || error_handler "Starting OpenCTI Containers"
     cd -
 }
