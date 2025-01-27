b62996bff2248fc57d635bff3e9a8a62982a393e
nobone-test <79396632+nobone-test@users.noreply.github.com>
2025-01-27 09:56:39 +0700

    Delete modules directory
    Signed-off-by: nobone-test <79396632+nobone-test@users.noreply.github.com>

--- modules/prerequisites/prerequisites.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-install_prerequisites() {
-    echo "🛠 Installing prerequisites..."
-    sudo apt update -y || error_handler "System Update"
-    sudo apt install -y git curl wget python3-pip || error_handler "Prerequisites Installation"
-}
-
--- modules/yara/yara_manual.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-install_yara_manual() {
-    echo "🚀 Installing YARA (manual step)..."
-    echo "YARA should be installed on endpoints individually."
-    echo "Use the following command on endpoints: sudo apt install -y yara"
-}
